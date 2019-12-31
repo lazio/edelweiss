9a87ca2c01239a50e66d324b5aaa3f2ddb8b2f10
Kapelianovych Yevhen <zhenja.96.lemok@ukr.net>
2019-12-31 08:51:30 +0200

    Add forward, back and reload methods to router. Make ability to provide regexp as a path in route.

--- src/router/router.mjs
+++ src/router/router.mjs
@@ -6,7 +6,7 @@ import Component from '../component/component.mjs'
 import { render } from '../render.mjs'
 
 export type Route = {
-  path: string,
+  path: string | RegExp,
   container: string,
   view: () => string | MTNode | Component | (string | MTNode | Component)[],
 }
@@ -18,11 +18,22 @@ export default class Router {
   constructor(routes: Route[]) {
     this._routes = routes
     // First route of the app.
-    this.current = routes.find(route => route.path === '/')
+    this.current = routes.find(route => {
+      const pathRegExp =
+        typeof route.path === 'string' ? new RegExp(route.path) : route.path
+
+      return pathRegExp.test('/')
+    })
   }
 
   async to(path: string): Promise<void> {
-    const route = this._routes.find(route => route.path === path)
+    const route = this._routes.find(r => {
+      const pathRegExp =
+        typeof r.path === 'string' ? new RegExp(`^${r.path}$`) : r.path
+
+      return pathRegExp.test(path)
+    })
+
     if (route) {
       const toContainer = document.querySelector(route.container)
 
@@ -38,5 +49,21 @@ export default class Router {
     } else {
       throw new Error(`No route is specified for path: ${path}!`)
     }
+  }
+
+  async reload() {
+    if (this.current) {
+      const { path, container, view } = this.current
+      await render(container, view())
+      window.history.replaceState({ path, container }, '', path)
+    }
+  }
+
+  back() {
+    window.history.back()
+  }
+
+  forward() {
+    window.history.forward()
   }
 }
