be897ef45205c8d8982c5e4c290e86d25ccd813c
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-10-27 20:57:39 +0200

    Separate library attributes names.

---
+++ src/utils/library_attributes.ts
@@ -0,0 +1,19 @@
+import { Hooks } from '../dom/hooks';
+
+const HOOK_ID_PREFIX = 'data-hook-id-';
+const EVENT_ID_PREFIX = 'data-event-id-';
+
+export function createHookAttributeName(name: Hooks): string {
+  return `${HOOK_ID_PREFIX}${name}`;
+}
+
+export function createEventIdAttributeName(eventId: number): string {
+  return `${EVENT_ID_PREFIX}${eventId}`;
+}
+
+export function isLibraryAttribute(attributeName: string): boolean {
+  return (
+    attributeName.startsWith(HOOK_ID_PREFIX) ||
+    attributeName.startsWith(EVENT_ID_PREFIX)
+  );
+}
